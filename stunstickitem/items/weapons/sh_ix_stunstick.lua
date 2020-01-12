caee9f8d90f38aa54e62e56466e728c7f42d71a0
LegendSMEfire <56114121+LegendSMEfire@users.noreply.github.com>
2020-01-12 21:07:39 +0100

    Add files via upload

---
+++ stunstickitem/items/weapons/sh_ix_stunstick.lua
@@ -0,0 +1,13 @@
+ITEM.name = "Stunstick"
+ITEM.description = "A 22 inch stick with a taser shape at the tip of the stick itself; it uses a 150KV function and can be used to neutralize a target during an impact."
+ITEM.model = "models/weapons/w_stunbaton.mdl"
+ITEM.class = "ix_stunstick"
+ITEM.weaponCategory = "melee"
+ITEM.isWeapon = true
+ITEM.width = 2
+ITEM.height = 1
+ITEM.iconCam = {
+	ang	= Angle(-0.23955784738064, 270.44906616211, 0),
+	fov	= 10.780103254469,
+	pos	= Vector(0, 200, 0)
+}
